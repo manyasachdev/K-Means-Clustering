718f03b1de959f657dfb420215cd6e8dbcf7c23d
singhpratham72 <singhpratham72@gmail.com>
2021-02-26 12:01:12 +0530

    Added centroid function

--- k-means algo.py
+++ k-means algo.py
@@ -1,9 +1,11 @@
 import math
 
 class Point:
-    def __init__(self, x, y):
+    def __init__(self, x=0, y=0):
         self.x = x
         self.y = y
+    def __str__(self):
+        return (f'({self.x}, {self.y})')
 
 #Example points
 p1=Point(20,5)
@@ -14,7 +16,18 @@ def E_Distance(P1, Pc):
     sum = math.sqrt(math.pow((P1.x-Pc.x),2) + math.pow((P1.y-Pc.y),2))
     return sum
 
+#Change the centroid
+def changeCentroid(p1, p2):
+    centroid = Point()
+    centroid.x = (p1.x + p2.x)/2
+    centroid.y = (p1.y + p2.y)/2
+    return centroid
+
+newCentroid = changeCentroid(p1, pc)
+
 print(E_Distance(p1, pc))
+print(newCentroid)
+
 
 
 
