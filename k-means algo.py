9486917d0c7c23c6c7069c9762a7eb968acc7ab2
manyasachdev <manyasachdev1@gmail.com>
2021-03-01 18:40:18 +0530

    ideal k?

--- k-means algo.py
+++ k-means algo.py
@@ -96,8 +96,12 @@ def main():
 
     # Adding data to a list of Points
     DataSet = []
-    for i in range(10):
+    for i in range(len(x)):
         DataSet.append(Point(x[i], y[i]))
+
+    for point in DataSet:
+        print(point) 
+
     #     plt.scatter(DataSet[i].x, DataSet[i].y, color = 'hotpink')
     # plt.show()
     # ------------------------------------------------------------------------------------------------------------------------------------
@@ -109,20 +113,25 @@ def main():
     SSEvals = []
     klist =[]
 
-    for m in range(1,6):
+    for m in range(1,11):
         K=m
 
         # clusters = List of Cluster objects
         clusters = []
-        colors = ["black", "red", "pink", "yellow", "blue"]
+        colors = ["black", "red", "pink", "yellow", "blue", "magenta", "orange", "brown", "green", "purple"]
 
         # Select random centroids and create a list of K clusters
         randomPoints = selectRandomPoints(K, DataSet)
+        
+        #print(f"Appointed centroid: {randomPoints}")
 
         # Creating and initializing the clusters using random centroids
         for i in range(K):
             clusterInstance = Cluster(centroid=randomPoints[i], color=colors[i])
             clusters.append(clusterInstance)
+        
+        for cluster in clusters:
+            print(f"HEREEEEEEEEEEEEEE({cluster.centroid.x}, {cluster.centroid.y})")
 
 
         # Iterate throught the dataset
@@ -164,11 +173,12 @@ def main():
         for j in range(len(cluster.points)):
             temp = calculateEuclideanDistance(cluster.points[j], cluster.centroid)
             temp = math.pow(temp, 2)
-            cluster.sse = cluster.sse + temp
+            cluster.sse = temp
         temp2 = temp2 + cluster.sse
         SSEvals.append(temp2)
         klist.append(i+1)
         print(f"SSE{i}={SSEvals[i]}")
+        print(f"Klist{i}={klist[i]}")
         i=i+1
     
     plt.plot(klist, SSEvals)
@@ -177,7 +187,7 @@ def main():
 """
     # PLOTTING CLUSTERS----------------------------------------------------------------------------------------------------------------
     # Printing the clusters
-    print("DatSet:")
+    print("DataSet:")
     for point in DataSet:
         print(point)
 
@@ -190,8 +200,6 @@ def main():
         print(f"Centroid = ({cluster.centroid.x}, {cluster.centroid.y})")
         cluster.plotCentroid()
         cluster.plotCluster()
-
-
 
     plt.show()
     plt.close()
